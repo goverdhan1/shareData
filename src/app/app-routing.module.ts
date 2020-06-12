3d23843820783eded13bb550ddead1caa1881118
Koyalkar <Goverdhan.Koyalkar@transformco.com>
2020-06-12 19:22:34 +0530

    sharedata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { SiblingComponent } from './sibling/sibling.component';
+import { ChildComponent } from './child/child.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'parent', component:ParentComponent},
+  {path:'sibling', component: SiblingComponent},
+  {path:'child', component: ChildComponent},
+  {path: '**', redirectTo:'/parent'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
